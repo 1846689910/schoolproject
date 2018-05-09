36d46ff0151062a5c23a8606ebc79dde34364428
1846689910 <zuhe75@gmail.com>
2018-05-08 17:13:17 -0700

    ...

--- src/utils/common/MD5.java
+++ src/utils/common/MD5.java
@@ -1,5 +1,6 @@
 package utils.common;
-
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 public class MD5 {
     public static void main(String[] args) {
         String s = "hello world";
@@ -9,14 +10,14 @@ public class MD5 {
     }
     public static String getMD5(byte[] buf) {
         try {
-            java.security.MessageDigest md = java.security.MessageDigest.getInstance("MD5");
+            MessageDigest md = MessageDigest.getInstance("MD5");
             byte[] array = md.digest(buf);
             StringBuilder sb = new StringBuilder();
             for (int i = 0; i < array.length; ++i) {
                 sb.append(Integer.toHexString((array[i] & 0xFF) | 0x100).substring(1,3));
             }
             return sb.toString();
-        } catch (java.security.NoSuchAlgorithmException e) {
+        } catch (NoSuchAlgorithmException e) {
             e.printStackTrace();
         }
         return null;
