e7ac75dbe684e23e0e67d97b3a55d01bc11a0f1c
1846689910 <zuhe75@gmail.com>
2018-05-21 00:30:39 -0700

    ...

--- src/utils/algorithm/LC1_100.java
+++ src/utils/algorithm/LC1_100.java
@@ -816,6 +816,33 @@ public class LC1_100 {
         }
         return end - start + 1;
     }
+    /**
+     * LC60 Permutation Sequence
+     * */
+    public String getPermutation(int n, int k) {
+        List<Integer> numbers = new ArrayList<>();
+        int[] factorial = new int[n + 1];
+        StringBuilder sb = new StringBuilder();
+        // create an array of factorial lookup
+        int sum = 1;
+        factorial[0] = 1;
+        for(int i = 1; i <= n; i ++){
+            sum *= i;
+            factorial[i] = sum;
+            // factorial[] = {1, 1, 2, 6, 24, ... n!}
+            // create a list of numbers to get indices
+            numbers.add(i);
+        }
+        // numbers = {1, 2, 3, 4}
+        k--;
+        for(int i = 1; i <= n; i++){
+            int index = k / factorial[n - i];
+            sb.append(String.valueOf(numbers.get(index)));
+            numbers.remove(index);
+            k -= index * factorial[n - i];
+        }
+        return sb.toString();
+    }
     @Test
     public void baseTest(){
         System.out.println(lengthOfLastWord("a "));
