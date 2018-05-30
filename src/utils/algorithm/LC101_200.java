189053b9fafd8e410dbd1a5c6e766ce4e8d67386
1846689910 <zuhe75@gmail.com>
2018-05-30 10:25:55 -0700

    ...

--- src/utils/algorithm/LC101_200.java
+++ src/utils/algorithm/LC101_200.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
+import java.util.Queue;
 
 public class LC101_200 {
     /**
@@ -49,6 +50,76 @@ public class LC101_200 {
         int leftHeight = maxDepth(root.left);
         int rightHeight = maxDepth(root.right);
         return Math.max(leftHeight, rightHeight) + 1;  // 根节点也算一层，也要加上
+    }
+    /**
+     * LC 106 Construct Binary Tree from Inorder and Postorder Traversal 中后重构
+     * */
+    public TreeNode inPostBuildTree (int[] in, int[] post) {
+        return inPostBuildTreeHelper(in, post, 0, 0, in.length);
+    }
+    private TreeNode inPostBuildTreeHelper(int[] in,int[] post,int idx1,int idx2,int inLen) {
+        if (inLen == 0) {
+            return null;
+        }
+        TreeNode root = new TreeNode(post[idx2 + inLen - 1]);
+        int leftLen = 0;
+        while (in[idx1 + leftLen] != post[idx2 + inLen - 1]) {
+            leftLen ++;
+        }
+        root.left = inPostBuildTreeHelper(in, post, idx1, idx2, leftLen);
+        root.right = inPostBuildTreeHelper(in, post,idx1 + leftLen + 1,idx2 + leftLen,inLen - leftLen - 1);
+        return root;
+    }
+    /**
+     * LC107 level order traverse 2 倒着排
+     * For example:
+     Given binary tree [3,9,20,null,null,15,7],
+     3
+     / \
+     9  20
+     /  \
+     15   7
+     return its bottom-up level order traversal as:
+     [
+     [15,7],
+     [9,20],
+     [3]
+     ]
+     * */
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        Deque<List<Integer>> result = new LinkedList<>();
+        if (root == null) return new ArrayList<>(result);
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+        while (! queue.isEmpty()) {
+            int size = queue.size();
+            List<Integer> curLayer = new ArrayList<>();
+            for (int i = 0; i < size; i ++) {
+                TreeNode cur = queue.poll();
+                if (cur.left != null) queue.offer(cur.left);
+                if (cur.right != null) queue.offer(cur.right);
+                curLayer.add(cur.value);
+            }
+            result.offerFirst(curLayer);
+        }
+        return new ArrayList<>(result);
+    }
+    /**
+     * LC108 Convert sorted array to BST
+     * */
+    public TreeNode sortedArrToBST(int[] arr) {
+        if (arr == null || arr.length == 0) return null;
+        TreeNode root = sortedArrToBSTHelper(arr, 0, arr.length - 1);
+        return root;
+    }
+
+    public TreeNode sortedArrToBSTHelper(int[] arr, int start, int end) {
+        if (start > end) return null;
+        int mid = start + (end - start) / 2;
+        TreeNode node = new TreeNode(arr[mid]);
+        node.left = sortedArrToBSTHelper(arr, start, mid - 1);
+        node.right = sortedArrToBSTHelper(arr, mid + 1, end);
+        return node;
     }
 
 }
