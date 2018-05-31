e9745703fcb2d689c9cafb16dd7300c45dddc90b
1846689910 <zuhe75@gmail.com>
2018-05-31 10:55:55 -0700

    ...

--- src/utils/algorithm/LC101_200.java
+++ src/utils/algorithm/LC101_200.java
@@ -1,6 +1,7 @@
 package utils.algorithm;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Deque;
 import java.util.LinkedList;
 import java.util.List;
@@ -174,7 +175,7 @@ public class LC101_200 {
         return result;
     }
 
-    public void pathSum2Helper(TreeNode root, int sum, List<List<Integer>> result, List<Integer> cur){
+    private void pathSum2Helper(TreeNode root, int sum, List<List<Integer>> result, List<Integer> cur){
         if(root == null) return;
         cur.add(root.value);
 
@@ -203,5 +204,170 @@ public class LC101_200 {
         if (root == null) return 0;
         return (root.value == sum ? 1 : 0)
                 + pathSum3Helper(root.left, sum - root.value) + pathSum3Helper(root.right, sum - root.value);
+    }
+    /**
+     * LC114 Flatten a binary tree to LinkedList
+     *1
+     / \
+     2   5
+     / \   \
+     3   4   6
+     The flattened tree should look like:
+    1-2-3-4-5-6
+     * */
+    public void flatten(TreeNode root){
+        flattenHelper(root, new TreeNode[]{null});
+    }
+    private void flattenHelper(TreeNode root, TreeNode[] prev) {
+        if (root == null)return;
+        flattenHelper(root.right, prev);
+        flattenHelper(root.left, prev);
+        root.right = prev[0];
+        root.left = null;
+        prev[0] = root;
+    }
+    /**
+     * LC116 Populating Next Right Pointers in Each Node
+     class TreeLinkNode{
+        TreeLinkNode left;
+        TreeLinkNode right;
+        TreeLinkNode next;
+        int val;
+        public TreeLinkNode(int val){
+            this.val = val;
+        }
+     }
+     设置每个节点的next指针指向相邻的下一节点
+     Given the following perfect binary tree,
+
+        1
+      /  \
+     2    3
+     / \  / \
+     4  5  6  7
+     After calling your function, the tree should look like:
+
+     1 -> NULL
+     /  \
+     2 -> 3 -> NULL
+     / \  / \
+     4->5->6->7 -> NULL
+     假定是完美的二叉树，所有父节点都有两个子节点
+     You may assume that it is a perfect binary tree (ie, all leaves are at the same level, and every parent has two children).
+     * */
+    public void connect(TreeLinkNode root) {
+        TreeLinkNode start = root;
+        while(start != null){
+            TreeLinkNode cur = start;
+            while(cur != null){
+                if(cur.left != null) cur.left.next = cur.right;
+                if(cur.right != null && cur.next != null) cur.right.next = cur.next.left;
+                cur = cur.next;
+            }
+            start = start.left;
+        }
+    }
+    /**
+     * LC117 同LC116，但没有完美节点的假设
+     * */
+    public void connect1(TreeLinkNode root) {
+        while(root != null){
+            TreeLinkNode dummy = new TreeLinkNode(0);
+            TreeLinkNode cur = dummy;
+            while(root != null){
+                if(root.left != null) {
+                    cur.next = root.left;
+                    cur = cur.next;
+                }
+                if(root.right != null) {
+                    cur.next = root.right;
+                    cur = cur.next;
+                }
+                root = root.next;
+            }
+            root = dummy.next;
+        }
+    }
+    /**
+     * LC118 Pascal's triangle
+     * 三角形外围都是1，内部每个数都是肩上两数之和
+     Input: 5
+     Output:
+     [
+     [1],
+     [1,1],
+     [1,2,1],
+     [1,3,3,1],
+     [1,4,6,4,1]
+     ]
+     * */
+    public List<List<Integer>> pascalTriangle(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        // First base case; if user requests zero rows, they get zero rows.
+        if (numRows == 0) {
+            return result;
+        }
+
+        // Second base case; first row is always [1].
+        result.add(Arrays.asList(1));
+
+        for (int i = 1; i < numRows; i ++) {
+            List<Integer> cur = new ArrayList<>();
+            List<Integer> prev = result.get(i - 1);
+
+            // The first row element is always 1.
+            cur.add(1);
+
+            // Each triangle element (other than the first and last of each row)
+            // is equal to the sum of the elements above-and-to-the-left and
+            // above-and-to-the-right.
+            for (int j = 1; j < i; j ++) {
+                cur.add(prev.get(j - 1) + prev.get(j));
+            }
+
+            // The last row element is always 1.
+            cur.add(1);
+
+            result.add(cur);
+        }
+        return result;
+    }
+    /**
+     * LC119 pascal triangle 2，同上要求
+     * 给定一个行号，返回该行的三角形数
+     * */
+    public List<Integer> pascalTriangle2(int idx) {
+        List<Integer> result = new ArrayList<>();
+        if (idx < 0) return result;
+        for (int i = 0; i < idx + 1; i ++) {
+            result.add(0, 1);
+            for (int j = 1; j < result.size() - 1; j ++) {
+                result.set(j, result.get(j) + result.get(j + 1));
+            }
+        }
+        return result;
+    }
+    /**
+     * LC120 triangle
+     * 给定一个List[List[Int]]的三角形, 找到顶到底的通路使得和最小
+     * */
+    public int minTotal(List<List<Integer>> triangle) {
+        int[] arr = new int[triangle.size() + 1];
+        for(int i = triangle.size() - 1; i >= 0; i --){
+            for(int j = 0; j < triangle.get(i).size(); j ++){
+                arr[j] = Math.min(arr[j], arr[j + 1]) + triangle.get(i).get(j);
+            }
+        }
+        return arr[0];
+    }
+}
+class TreeLinkNode{
+    TreeLinkNode left;
+    TreeLinkNode right;
+    TreeLinkNode next;
+    int val;
+    public TreeLinkNode(int val){
+        this.val = val;
     }
 }
